7d3f0a16f6e9f1ad0882fa8f7d787f3ce704e824
dhananjayansb <dhananjayansb@gmail.com>
2021-07-17 22:43:10 +0530

    update

--- DataDriven2/src/TestCases/driveTest1.java
+++ DataDriven2/src/TestCases/driveTest1.java
@@ -1,38 +1,104 @@
 package TestCases;
 
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
-import java.util.Set;
+import java.util.function.Function;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 
 public class driveTest1 
 {
 	static WebDriver driver;
 	private static final String CSV = "D:\\\\git\\\\Selenium-Projects\\\\DataDriven2\\\\Data.csv";
 		
-		public static void main(String[] args) throws IOException, InterruptedException 
+	public static void pleasewait()
+	{
+		//Loading-icon-wait
+				WebElement waitElement = null;
+				Wait<WebDriver> fwait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(100)).pollingEvery(Duration.ofMillis(600)).ignoring(NoSuchElementException.class);
+				try 
+				{
+					  waitElement = fwait.until(new Function<WebDriver, WebElement>() 
+					  {
+					   public WebElement apply(WebDriver driver) 
+					   {
+					      return driver.findElement(By.cssSelector("div.loading.fixed-loading"));
+					   }
+					  }); 	
+				} catch (Exception e) {}
+				
+				if (waitElement != null) 
+				{
+					  WebDriverWait wait = new WebDriverWait(driver, 60);
+				      wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.loading.fixed-loading")));
+				}
+	}
+	
+	public static void loading()
+	{
+		//Loading-icon-wait
+				WebElement waitElement = null;
+				Wait<WebDriver> fwait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(100)).pollingEvery(Duration.ofMillis(600)).ignoring(NoSuchElementException.class);
+				try 
+				{
+					  waitElement = fwait.until(new Function<WebDriver, WebElement>() 
+					  {
+					   public WebElement apply(WebDriver driver) 
+					   {
+					      return driver.findElement(By.cssSelector("div.progress-box"));
+					   }
+					  }); 
+				} catch (Exception e) {}
+				
+				if (waitElement != null) 
+				{
+					  WebDriverWait wait = new WebDriverWait(driver, 60);
+				      wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.progress-box")));
+				}
+	}
+	
+	public static void output(File file,String message) throws IOException
+	{
+		FileWriter output = new FileWriter(file,true);
+		BufferedWriter buffer = new BufferedWriter(output);
+		buffer.write(message+"\n");
+		buffer.close();
+	}
+		public static void main(String[] args) throws IOException, InterruptedException, CsvValidationException 
 		{
-			Logger logger = Logger.getLogger("driveTest3");
-			PropertyConfigurator.configure("Log4j.properties");
+			//Logger logger = Logger.getLogger("driveTest3");
+			//PropertyConfigurator.configure("Log4j.properties");
+			
+			
 			System.setProperty("webdriver.chrome.driver","D:\\software\\Selenium\\Drivers\\chromedriver.exe");
 			driver = new ChromeDriver();
 			driver.manage().window().maximize();
@@ -44,149 +110,171 @@ public class driveTest1
 			WebDriverWait wait = new WebDriverWait(driver, 30);
 			Actions action = new Actions(driver);
 			elementLocators ELPage = new elementLocators(driver);
-			logger.info("Browser opened and maximized");
-			
+			//logger.info("Browser opened and maximized");
+			//output("Browser opened and maximized");
 			
 			driver.get("https://demo.admanagerplus.com/");
-			logger.info("URL opened");
+			//logger.info("URL opened");
+			//output("URL opened");
 			
 			//Administrator-button
 			driver.findElement(By.partialLinkText("Administrat")).click();
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//ul[@id='TABS_AREA']")));
-			logger.info("Logged in Adminstrator");	
 			
 			//Management-button
 			WebElement topMenu = driver.findElement(By.id("top-menu"));
 			action.moveToElement(topMenu).moveToElement(driver.findElement(By.xpath("//a[contains(text(),'Management')]"))).click().build().perform();
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Create Single User')]")));
-			logger.info("Logged in Management");
+			//logger.info("Logged in Management");
 			
-			//Create-User-button
-			action.moveToElement(topMenu).moveToElement(driver.findElement(By.xpath("//span[contains(text(),'Create Single User')]"))).click().build().perform();
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@name='save']")));
-			logger.info("Logged in Create User");
+			CSVReader reader = null;
 			
-			File file = new File(CSV);
-			byte[] bytes = FileUtils.readFileToByteArray(file);
-			String data = new String(bytes);
-			
-			data = StringUtils.replace(data,"\r","");
-			String[] dataArray = data.split("\n");
-		
-			String keys = dataArray[0];
-			List<String> keyFromFile = new ArrayList<>();
-			String[] keyArr = keys.split(",");
+			reader = new CSVReader(new FileReader(CSV));
+			String[] header = reader.readNext();
+			String[] csv = null;
 			ArrayList<HashMap<String, String>> array = new ArrayList<HashMap<String, String>>();
-			
-			keyFromFile.addAll(Arrays.asList(keyArr));
-			keyFromFile.remove(0);
-			long startTime = 0;
-			for(int d=1;d<dataArray.length;d++)
+				
+			while((csv = reader.readNext()) != null)
 			{
 				HashMap<String, String> mp = new HashMap<>();
-				List<String> row = new ArrayList<>();
-				String[] rowArr = dataArray[d].split(",");
-				row.addAll(Arrays.asList(rowArr));
-				row.remove(0);
-				
-				//captures single row
-				for(int i=0;i<keyFromFile.size();i++)
+				for(int i=0;i<header.length;i++)
 				{
-					mp.put(keyFromFile.get(i).trim(),row.get(i).trim());
+					mp.put(header[i], csv[i]);
 				}
 				array.add(mp);
-			}
-			
+			}  
+			int count = 1;
 			for (HashMap<String, String> hm : array)
 			{
+				System.out.print(count);
+				String filePath = "D:\\git\\Selenium-Projects\\DataDriven2";
+				String fileName = "result"+count+".txt";
+				File file = new File(filePath+"\\"+fileName);
+				//Create-User-button
+				try
+				{
+					wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Create Single User')]")));
+					action.moveToElement(driver.findElement(By.xpath("//span[contains(text(),'Create Single User')]"))).click().build().perform();
+					pleasewait();
+					//logger.info("Logged in Create User");
+					output(file,"Logged in Create User");
+				}
+				catch(Exception e) {e.printStackTrace();}
+				long startTime = System.currentTimeMillis();
+				
 				List<String> headlist = new ArrayList<String>(hm.keySet());
 				List<String> valuelist = new ArrayList<String>(hm.values());
-				startTime = System.currentTimeMillis();
+				 
 				for (int i = 0; i < headlist.size(); i++)
 				{
 					String key = headlist.get(i);
 					if(key.equals("First Name"))
 		            {
 		            	ELPage.setFirstName(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered FirstName");
+		            	//logger.info("Entered FirstName");
+		            	output(file,"Entered FirstName");
 		            }
 					else if(key.equals("Initials"))
 		            {
 		            	ELPage.setInital(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered Initial");
+		            	//logger.info("Entered Initial");
+		            	output(file,"Entered Inital");
 		            }
 		            else if(key.equals("Last Name"))
 		            {
 		            	ELPage.setLastName(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered LastName");
+		            	//logger.info("Entered LastName");
+		            	output(file,"Entered LastName");
 		            }
 		            else if(key.equals("Logon Name"))
 		            {
 		            	ELPage.setLogonName(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered LogonName");
+		            	//logger.info("Entered LogonName");
+		            	output(file,"Entered LogonName");
 		            }
 		            else if(key.equals("SAM Account Name"))
 		            {
 		            	ELPage.setPrewindows(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered Prewindows");
+		            	//logger.info("Entered Prewindows");
+		            	output(file,"Entered Prewindows");
 		            }
 		            else if(key.equals("Full Name"))
 		            {
 		            	ELPage.setFullName(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered FullName");
+		            	//logger.info("Entered FullName");
+		            	output(file,"Entered FullName");
 		            }
 		            else if(key.equals("Display Name"))
 		            {
 		            	ELPage.setDisplayName(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered DisplayName");
+		            	//logger.info("Entered DisplayName");
+		            	output(file,"Entered DisplayName");
 		            }
 		            else if(key.equals("Employee ID"))
 		            {
 		            	ELPage.setEmployeeId(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered EmployeeId");
+		            	//logger.info("Entered EmployeeId");
+		            	output(file,"Entered EmployeeID");
 		            }
 		            else if(key.equals("Description"))
 		            {
 		            	ELPage.setDescription(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered Description");
+		            	//logger.info("Entered Description");
+		            	output(file,"Entered Description");
 		            }
 		            else if(key.equals("Office"))
 		            {
 		            	ELPage.setOffice(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered Office");
+		            	//logger.info("Entered Office");
+		            	output(file,"Entered Office");
 		            }
 		            else if(key.equals("Telephone Number"))
 		            {
 		            	ELPage.setPhone(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered Phone");
+		            	//logger.info("Entered Phone");
+		            	output(file,"Entered Phone");
 		            }
 		            else if(key.equals("Email Address"))
 		            {
 		            	ELPage.setEmail(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered Email");
+		            	//logger.info("Entered Email");
+		            	output(file,"Entered Email");
 		            }
 		            else if(key.equals("Webpage"))
 		            {
 		            	ELPage.setWebpage(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered Web Page");
+		            	//logger.info("Entered Web Page");
+		            	output(file,"Entered WebPage");
 		            }
 		            else if(key.equals("Container Name"))
 		            {
 		            	ELPage.setContainer(String.valueOf(valuelist.get(i)));
-		            	logger.info("Entered Container value");
+		            	//logger.info("Entered Container value");
+		            	output(file,"Entered Container Value");
 		            }
 				}
 				
 				driver.findElement(By.name("save")).sendKeys(Keys.PAGE_DOWN);
 				driver.findElement(By.name("save")).click();
 				String result = driver.findElement(By.id("statusTable")).getText();
-				logger.error(result);
+				//logger.error(result);
+				output(file,result);
 				System.out.println("complete");
 			
 				long endTime = System.currentTimeMillis();
-				long totalTime = endTime - startTime;
-				logger.info("-----------COMPLETED TEST CASE SUCCESSFULLY---------");
-				logger.info("Total Page Load Time: " + totalTime + "milliseconds");
+				float totalTime = (endTime - startTime)/1000F;
+				//logger.info("-----------COMPLETED TEST CASE SUCCESSFULLY---------");
+				output(file,"-----------COMPLETED TEST CASE SUCCESSFULLY---------");
+				//logger.info("Total Page Load Time: " + totalTime + "Seconds");
+				output(file,"Total Page Load Time: " + totalTime + "Seconds");
+				
+				//Next-user-entry-creation
+				driver.findElement(By.name("cancel")).sendKeys(Keys.PAGE_DOWN);
+				driver.findElement(By.name("cancel")).click();
+				Thread.sleep(1000);
+				driver.findElement(By.cssSelector("#alert_ok")).click();
+				Thread.sleep(2000);
+				count++;
 			}
 	}
 }
--- DataDriven2/src/TestCases/driveTest3.java
+++ DataDriven2/src/TestCases/driveTest3.java
@@ -3,7 +3,10 @@ package TestCases;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -14,21 +17,25 @@ import org.apache.pdfbox.util.PDFTextStripper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import java.time.Duration;
 
 
 public class driveTest3 
 {
 	static WebDriver driver;
 	static JavascriptExecutor js = (JavascriptExecutor) driver;
-	static String pdf = "D:\\git\\Selenium-Projects\\DataDriven2\\AuditReport.pdf";
-	
+	static String pdf = "D:\\git\\Selenium-Projects\\DataDriven2\\download\\";
+	static WebElement Loading;
 	public static void enter(String element,String data) throws IOException
 	{
 		WebDriverWait wait = new WebDriverWait(driver, 30);
@@ -56,12 +63,56 @@ public class driveTest3
 		catch(Exception e) {e.printStackTrace();}
 	}
 	
-	public static void main(String[] args) throws IOException, InterruptedException
+	public static boolean isFileDownloaded(String filePath, String fileName) throws Exception {
+	    final int SLEEP_TIME_MILLIS = 1000;
+	    File file = new File(filePath+"\\"+fileName);
+	    final int timeout = 60* SLEEP_TIME_MILLIS;
+	    int timeElapsed = 0;
+	    while (timeElapsed<timeout){
+	        if (file.exists()) {
+	            System.out.println(fileName + " is present");
+	            return true;
+	        } else {
+	            timeElapsed +=SLEEP_TIME_MILLIS;
+	            Thread.sleep(SLEEP_TIME_MILLIS);
+	        }
+	    }
+	    return false;
+	}
+	
+	public static void loading()
 	{
+		//Loading-icon-wait
+				WebElement waitElement = null;
+				Wait<WebDriver> fwait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(100)).pollingEvery(Duration.ofMillis(600)).ignoring(NoSuchElementException.class);
+				try 
+				{
+					  waitElement = fwait.until(new Function<WebDriver, WebElement>() 
+					  {
+					   public WebElement apply(WebDriver driver) 
+					   {
+					      return driver.findElement(By.cssSelector("div.loading"));
+					   }
+					  });
+				} catch (Exception e) {}
+				
+				if (waitElement != null) 
+				{
+					  WebDriverWait wait = new WebDriverWait(driver, 60);
+				      wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.loading")));
+				}
+	}
+	public static void main(String[] args) throws Exception
+	{
+		ChromeOptions options = new ChromeOptions();
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("download.default_directory", pdf);
+		options.setExperimentalOption("prefs", prefs);
+		
 		Logger logger = Logger.getLogger("driveTest4");
 		PropertyConfigurator.configure("Log4j.properties");
 		System.setProperty("webdriver.chrome.driver","D:\\software\\Selenium\\Drivers\\chromedriver.exe");
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
@@ -70,12 +121,11 @@ public class driveTest3
 		//Explicit-wait
 		WebDriverWait wait = new WebDriverWait(driver, 30);
 		Actions action = new Actions(driver);
-		elementLocators ELPage = new elementLocators(driver);
 		logger.info("Browser opened and maximized");
 		
 		driver.get("https://demo.admanagerplus.com/");
 		logger.info("URL opened");
-		
+
 		//Administrator-button
 		driver.findElement(By.partialLinkText("Administrat")).click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//ul[@id='TABS_AREA']")));
@@ -89,14 +139,16 @@ public class driveTest3
 		
 		//Audit-Report-Button
 		action.moveToElement(topMenu).moveToElement(driver.findElement(By.xpath("//li[@id='module_7003']/a[1]"))).click().build().perform();
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#ResultData_auditReport")));
+		loading();
 		logger.info("Audit report opened");
 		
+		
 		//Sorting
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='admp-wid-auto']//a)[1]")));
+		loading();
 		action.moveToElement(driver.findElement(By.name("daterange"))).click().perform();
 	    action.moveToElement(driver.findElement(By.cssSelector("#mCSB_9_container > li:nth-child(6)"))).click().perform();
 	    driver.findElement(By.id("generateReport")).click();
+	    loading();
 	    
 	    //Remove-Attribute
 	    Thread.sleep(10000);
@@ -107,18 +159,20 @@ public class driveTest3
 	    action.keyDown(Keys.CONTROL).moveToElement(status).click().moveToElement(actionname).click().keyUp(Keys.CONTROL).perform();
 	    action.moveToElement(driver.findElement(By.cssSelector("i.admp-icon.icn-backward"))).click().perform();
 	    action.moveToElement(driver.findElement(By.xpath("(//button[text()='OK'])[1]"))).click().perform();
+	    loading();
 	    logger.info("Removed Attribute");
 	    Thread.sleep(10000);
 	    
 	    //Add-Attribute
 	    action.moveToElement(driver.findElement(By.cssSelector("i.admp-icon.icn-addRemove"))).click().perform();
-	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//button[text()='OK'])[1]")));
+	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//button[text()='OK'])[1]")));	    
 	    WebElement statusadd = driver.findElement(By.xpath("//ul[@id='availableCols_1']//li[1]"));
 	    WebElement actionnameadd = driver.findElement(By.xpath("//ul[@id='availableCols_1']//li[2]"));
 	    action.keyDown(Keys.CONTROL).moveToElement(statusadd).click().moveToElement(actionnameadd).click().keyUp(Keys.CONTROL).perform();
 	    Thread.sleep(1000);
 	    action.moveToElement(driver.findElement(By.cssSelector("i.admp-icon.icn-forward"))).click().perform();
 	    action.moveToElement(driver.findElement(By.xpath("(//button[text()='OK'])[1]"))).click().perform();
+	    loading();
 	    logger.info("Added Attribute");
 	    
 	    //Search and export as PDF
@@ -129,17 +183,19 @@ public class driveTest3
 	    enter("input#searchValue_auditReport_OBJECT_NAME","Dhananjayan");
 
 	    dropdown("//tr[@id='searchCol_auditReport']//td[2]//div//button[@title='--Select Attribute--']","//span[text()='Create Users']");
-	    Thread.sleep(5000);
+	    loading();
 	    dropdown("//tr[@id='searchCol_auditReport']//td[3]//div//button[@title='--Select Attribute--']","//span[text()='AD Management']");
-	    Thread.sleep(5000);
+	    loading();
 	    dropdown("//tr[@id='searchCol_auditReport']//td[7]//div//button[@title='--Select Attribute--']","//span[text()='Create Single User']");
-	    Thread.sleep(5000);
+	    loading();
 	    action.moveToElement(driver.findElement(By.cssSelector("i[data-original-title='Export as']"))).click().perform();
 	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("PDF")));
 	    action.moveToElement(driver.findElement(By.linkText("PDF"))).click().perform();
+	    Thread.sleep(5000);
+	    isFileDownloaded(pdf,"AuditReport.pdf"); 
 	    
-	    
-	    File file = new File(pdf);
+	    String path = ("D:\\git\\Selenium-Projects\\DataDriven2\\download\\AuditReport.pdf");
+	    File file = new File(path);
 		FileInputStream fis = new FileInputStream(file);
 		PDFParser parser = new PDFParser(fis);
 		parser.parse();
